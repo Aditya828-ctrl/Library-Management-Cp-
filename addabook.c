5ce151520e33702aed57f14b20b87bfe549c716d
tanish4067 <tanishmittal0658@gmail.com>
2020-06-04 02:43:28 +0530

    ufff!!! run these please read comments properly and edit comments if there is typo

--- addabook.c
+++ addabook.c
@@ -8,26 +8,24 @@ typedef struct bklis{
   char Genre[30];
   int issue;
   struct bklis *next;
-
-}bklist;
-bklist *loaddatabase()// declaring  a function of type bklist which returns a pointer
+}bklist;//declared a structure of type bklist
+bklist *addabook(bklist *z)// declaring  a function of type bklist which returns a pointer
 //this function takes in a string pointer as a paramter
 {
-
-  FILE *aut,*tit,*gen,*iss;
+  FILE *aut,*tit,*gen,*iss; // declaring file pointer
   bklist *header,*point,*commit;
-  int count;
-  aut = fopen("AUTHOR.txt","r");
-  tit = fopen("TITLE.txt","r");
-  gen = fopen("Genre.txt","r");
-  iss = fopen("issue_no.txt","r");
+  int count,issre;
+  aut = fopen("AUTHOR.txt","r+");
+  tit = fopen("TITLE.txt","r+");
+  gen = fopen("Genre.txt","r+");
+  iss = fopen("issue_no.txt","r+");
   header = (bklist*) malloc(sizeof(bklist));//allocating dynamic memory of size of bklist structure
-  if(header == NULL)//this checks if malloc is able to allocate  memory or not
+  if(header == NULL) //this checks if malloc is able to allocate  memory or not
    {
        printf("Unable to allocate memory.");
        exit(0);
    }
-   fgets(header->auth,49,aut);//gets first line from files this "file pointers" are pointing to.
+   fgets(header->auth,49,aut); //gets first line from files this "file pointers" are pointing to.
    fgets(header->Title,59,tit);//gets first line from files this "file pointers" are pointing to.
    fgets(header->Genre,29,gen);//gets first line from files this "file pointers" are pointing to.
    fscanf(iss,"%d\n",&header->issue);//gets first line from file issue_no.txt as an integer
@@ -42,8 +40,8 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
    //              | issue|
    //              | next | ---pointing to----> NULL
    //              ￣￣￣￣
-   point = header;//assigning header address to point
-  for(count =2;;count++)// count is keeping track of no. of nodes
+  point = header; //assigning header address to point
+  for(count =2;;count++) // count is keeping track of no. of nodes
   { commit = (bklist*)malloc(sizeof(bklist));
     if(commit == NULL)// if system is not able to allocate memory this condition keeps a check on that
        {
@@ -52,13 +50,14 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
        }
        if (fgets(commit->Title,59,tit) == NULL)// fgets reads a new line each time for every iteration of loop
        //and if the new line doesn't points to anything i.e. emptyline then is condition breaks the loop
-       { free(commit);// done to avoid memory leak
-         commit = NULL;// to avoid dangling pointer problem
-         break;
+       { free(commit); // done to avoid memory leak
+         commit = NULL; // to avoid dangling pointer problem
+         break; // breaking loop
        }
     fgets(commit->auth,49,aut);
     fgets(commit->Genre,29,gen);
     fscanf(iss,"%d\n",&commit->issue);
+    issre = commit->issue;
     commit->next = NULL;
     //all the fgets and fscanf are taking value from newline for every iteration of loop
     //               commit
@@ -69,7 +68,6 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
     //              | issue|
     //              | next | ---pointing to----> NULL
     //              ￣￣￣￣
-
     point->next = commit;
     //               point                       commit
     //               _______                ➚    _______
@@ -82,8 +80,7 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
     //              point-> is pointing to commit which links commit to linked list
     //              and now commit->next is pointing to NULL
 
-    point = point->next;
-    //now in each iteration of loop point's adress is changed to where point->next is
+    point = point->next;//now in each iteration of loop point's adress is changed to where point->next is
     //pointing to i.e to commit structure. this helps in adding entire data in linked list format
     //               point                        point(after 1st iteration) point(after 2nd iteration)  commit
     //               _______                ➚    _______                ➚   _______                ➚   _______
@@ -97,17 +94,24 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
     // this printf below is just for checking the code
     // printf("%s %s %s %d\n",point->auth,point->Title,point->Genre,point->issue);
 
-
   }
+  fprintf(aut,"%s\n",z->auth);
+  fprintf(tit,"%s\n",z->Title);
+  fprintf(gen,"%s\n",z->Genre);
+  fprintf(iss,"%d\n",issre+1);
+  // this printf below is just for checking the code
+  // printf("%s %s %s %d\n",z->auth,z->Title,z->Genre,issre+1);
   return header;
 
 }
 int main()
 {
 bklist *a;
-a = loaddatabase();
-// function loads the database in linked list and then returns the header which helps in access of the linked list
+bklist *c;
+c = (bklist*)malloc(sizeof(bklist));
+scanf("%s\n%s\n%s",c->auth,c->Title,c->Genre);
+a = addabook(c);// function takes in string pointer as parameter and returns the header which helps in access of the linked list
 // this printf below is just for checking the code
- printf("%s",a->next->next->next->Title);
+// printf("%s",a->next->auth);
 
 }
