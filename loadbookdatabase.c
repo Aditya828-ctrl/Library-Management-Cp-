49235ff9ad0208c043a76dbce328c83bb272c803
tanish4067 <tanishmittal0658@gmail.com>
2020-06-07 16:36:54 +0530

    updated for count

--- loadbookdatabase.c
+++ loadbookdatabase.c
@@ -7,6 +7,7 @@ typedef struct bklis{
   char Title[60];
   char Genre[30];
   int issue;
+  int count;
   struct bklis *next;
 
 }bklist;
@@ -14,26 +15,28 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
 //this function takes in a string pointer as a paramter
 {
 
-  FILE *aut,*tit,*gen,*iss;
+  FILE *aut,*tit,*gen,*iss,*cnt;
   bklist *header,*point,*commit;
   int count;
   aut = fopen("AUTHOR.txt","r");
   tit = fopen("TITLE.txt","r");
   gen = fopen("Genre.txt","r");
   iss = fopen("issue_no.txt","r");
+  cnt = fopen("Count.txt","r");
   header = (bklist*) malloc(sizeof(bklist));//allocating dynamic memory of size of bklist structure
   if(header == NULL)//this checks if malloc is able to allocate  memory or not
    {
        printf("Unable to allocate memory.");
        exit(0);
    }
-   fgets(header->auth,49,aut);//gets first line from files this "file pointers" are pointing to.
-   fgets(header->Title,59,tit);//gets first line from files this "file pointers" are pointing to.
-   fgets(header->Genre,29,gen);//gets first line from files this "file pointers" are pointing to.
-   fscanf(iss,"%d\n",&header->issue);//gets first line from file issue_no.txt as an integer
-   // this printf below is just for checking the code
-   // printf("%s %s %s %d\n",header->auth,header->Title,header->Genre,header->issue);
-   header->next = NULL;
+   // fgets(header->auth,49,aut);//gets first line from files this "file pointers" are pointing to.
+   // fgets(header->Title,59,tit);//gets first line from files this "file pointers" are pointing to.
+   // fgets(header->Genre,29,gen);//gets first line from files this "file pointers" are pointing to.
+   // fscanf(iss,"%d\n",&header->issue);//gets first line from file issue_no.txt as an integer
+   // fscanf(cnt,"%d\n",&header->count);//gets first line from file issue_no.txt as an integer
+   // // this printf below is just for checking the code
+   // // printf("%s %s %s %d\n",header->auth,header->Title,header->Genre,header->issue);
+   // header->next = NULL;
    //               header
    //               _______
    //              | auth |
@@ -47,7 +50,7 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
   { commit = (bklist*)malloc(sizeof(bklist));
     if(commit == NULL)// if system is not able to allocate memory this condition keeps a check on that
        {
-           printf("memory can't be allocated please delete some memory");
+           printf("memory can't be allocated please free some memory");
            break;
        }
        if (fgets(commit->Title,59,tit) == NULL)// fgets reads a new line each time for every iteration of loop
@@ -59,6 +62,7 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
     fgets(commit->auth,49,aut);
     fgets(commit->Genre,29,gen);
     fscanf(iss,"%d\n",&commit->issue);
+    fscanf(cnt,"%d\n",&commit->count);
     commit->next = NULL;
     //all the fgets and fscanf are taking value from newline for every iteration of loop
     //               commit
@@ -103,6 +107,7 @@ bklist *loaddatabase()// declaring  a function of type bklist which returns a po
   fclose(tit);
   fclose(gen);
   fclose(iss);
+  fclose(cnt);
   return header;
 
 }
@@ -112,6 +117,6 @@ bklist *a;
 a = loaddatabase();
 // function loads the database in linked list and then returns the header which helps in access of the linked list
 // this printf below is just for checking the code
- printf("%s",a->next->next->next->Title);
+ printf("%d",a->next->next->next->count);
 
 }
